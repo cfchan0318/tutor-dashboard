4710062ed73d7694d142915f138d3e0067940263
Marco Chan <cf.chan0318@gmail.com>
2022-02-23 23:22:33 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-//app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
@@ -19,5 +19,6 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
+    console.log(path.resolve(__dirname, '../client/build', 'index.html'));
     console.log(`Server listening on ${PORT}`);
 });
