592e93a79daf1f9b640fb2df25e7ddbd45a871c8
Marco Chan <cf.chan0318@gmail.com>
2022-02-23 23:06:07 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -17,7 +17,3 @@ app.get("/api", (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
-
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
